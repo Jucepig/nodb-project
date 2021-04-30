4332ae8fa21f60ebea90c0867aad5cb0618271e1
Joe Fatani <jucepig@gmail.com>
2021-04-30 17:03:17 -0600

    completed html/JSX and CSS layout for page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ function App() {
   return (
     <div className="App">
       <header id="header" className="flex-row">
-        <h3>WatchLst</h3>
+        <h3>WATCHLiiST</h3>
       </header>
 
       <div id="main-container" className="flex-row">
         <div id="main" className="flex-row">
-          
+
           <aside id="show-form" className="flex-column">
             <h5>Add A Show!</h5>
             <div className="flex-column">
@@ -22,8 +22,23 @@ function App() {
             </div>
           </aside>
 
-          <section id="show-list">
-
+          <section id="show-list" className="flex-column">
+            <header id="wl-header">WATCHLiiST</header>
+            <div id="wl-list" className="flex-column">
+              <div className="show-item flex-row">
+                <div className="si-btns flex-column">
+                  <button>Edit</button>
+                  <button>Del</button>
+                </div>
+                <div className="show-details flex-row">
+                  <span>Title</span>
+                  <span>Genre</span>
+                  <span>Mins/Episode</span>
+                  <span>Runtime</span>
+                </div>
+              </div>
+            </div>
+            <footer id="wl-footer">TOTAL RUNTIME:</footer>
           </section>
 
         </div>
